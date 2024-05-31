f18e49936745a3ac5970db11551b71c06855628e
Madhav <guptamadhav69@gmail.com>
2024-06-01 05:27:50 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopLeftComponent } from './components/top-left/top-left.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { RadioComponent } from './components/radio/radio.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    TopLeftComponent, 
+    SidebarComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    ArtistsComponent, AlbumsComponent, RadioComponent, ChartsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
